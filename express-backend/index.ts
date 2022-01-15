4c55bceb0b3f516b885b4445bb4b3e3d15b117f5
Joseph Micla <josephmicla@gmail.com>
2022-01-15 13:51:29 -0800

    connects server to mongoose

--- express-backend/index.ts
+++ express-backend/index.ts
@@ -1,16 +1,26 @@
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
 dotenv.config();
 
+// Constants
 const PORT = process.env.PORT || 8080;
 
+// Server
 import express from 'express';
+import mongoose from 'mongoose';
 const morgan = require('morgan');
 
 const app = express();
+
+// Middleware
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to NOT =D');
+  mongoose
+    .connect(`${process.env.DB_URI}`)
+    .then((response) => {
+      res.send('Connected to DB successfully!');
+    })
+    .catch((err) => res.send(`Could not connect due to ${err}`));
 });
 
 app.listen(PORT, () => console.log(`API server running on port: ${PORT}`));
