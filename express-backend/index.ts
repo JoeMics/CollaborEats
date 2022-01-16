f8243a6cb44053cfc4af96d891d25221139e0e7e
Ivan <ivanxintang@gmail.com>
2022-01-15 16:44:23 -0800

    Import User schema and add test routes to seed the database

--- express-backend/index.ts
+++ express-backend/index.ts
@@ -11,8 +11,11 @@ const morgan = require('morgan');
 
 const app = express();
 
+import User from "./models/user";
+
 // Middleware
 app.use(morgan('tiny'));
+
 
 app.get('/', (req, res) => {
   mongoose
@@ -21,6 +24,24 @@ app.get('/', (req, res) => {
       res.send('Connected to DB successfully!');
     })
     .catch((err) => res.send(`Could not connect due to ${err}`));
+});
+
+// Get all users
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+
+// Add a user
+app.get("/add-user", async (req, res) => {
+  const newUser = await User.create({
+    _id: '2a',
+    email: 'JoeBics@example.com',
+    password: 'password',
+    avatar: 'https://avatars.githubusercontent.com/u/46177831?v=4'
+  })
+
+  res.send(newUser);
 });
 
 app.listen(PORT, () => console.log(`API server running on port: ${PORT}`));
