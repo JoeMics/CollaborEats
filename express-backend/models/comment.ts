7a8dfac9f3416517e1f80c00015f2c94c9b62282
Joseph Micla <josephmicla@gmail.com>
2022-01-15 19:54:29 -0800

    bugfix: optional _id for seeding

--- express-backend/models/comment.ts
+++ express-backend/models/comment.ts
@@ -2,15 +2,17 @@ import mongoose from 'mongoose';
 import { Schema } from 'mongoose';
 
 interface Comment {
-  ownerId: Number;
-  recipeId: Number;
+  _id?: String;
+  ownerId: String;
+  recipeId: String;
   content: String;
-};
+}
 
 const commentSchema = new Schema<Comment>({
-  ownerId: {type: Number, required: true},
-  recipeId: { type: Number, required: true},
-  content: {type: String, required: true},
+  _id: String,
+  ownerId: { type: String, required: true },
+  recipeId: { type: String, required: true },
+  content: { type: String, required: true },
 });
 
-export default mongoose.model("Comment", commentSchema);
+export default mongoose.model('Comment', commentSchema);
--- express-backend/models/recipe.ts
+++ express-backend/models/recipe.ts
@@ -4,29 +4,34 @@ import { Schema, Types } from 'mongoose';
 interface Ingredient {
   ingredient: String;
   amount: Number;
-  unitOfMeasure?: String
+  unitOfMeasure?: String;
 }
 
 interface Recipe {
+  _id?: String;
   path: Types.Array<String>;
   parent?: String;
-  ownerId: Number;
+  ownerId: String;
   title: String;
   description: String;
   ingredients: Types.Array<Ingredient>;
   instructions: String;
   photo: String;
-};
+}
 
 const recipeSchema = new Schema<Recipe>({
-    path: { type: [String], required: true },
-    parent: { type: String },
-    ownerId: { type: Number, required: true },
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    ingredients: { type: [{ingredient: String, amount: Number, unitOfMeasure: String}], required: true },
-    instructions: { type: String, required: true },
-    photo: { type: String },
+  _id: String,
+  path: { type: [String], required: true },
+  parent: { type: String },
+  ownerId: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  ingredients: {
+    type: [{ ingredient: String, amount: Number, unitOfMeasure: String }],
+    required: true,
+  },
+  instructions: { type: String, required: true },
+  photo: { type: String },
 });
 
-export default mongoose.model("Recipe", recipeSchema);
+export default mongoose.model('Recipe', recipeSchema);
