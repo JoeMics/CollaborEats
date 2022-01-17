475d8dd3a7e33b2f1b2528cff269e4d61ac7b21e
Ivan <ivanxintang@gmail.com>
2022-01-17 09:22:03 -0800

    Implement recipe routes

---
+++ server/routes/recipe.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import Recipe from '../models/recipe';
+const router = express.Router();
+
+
+router.get('/', async (req,res) => {
+  const recipes = await Recipe.find( {parent: null});
+  res.send(recipes);
+});
+
+router.get('/:id', async (req, res) => {
+  const recipe = await Recipe.find( {_id: req.params.id})
+  const recipeTree = await Recipe.find( {path: req.params.id })
+  res.send({recipe, recipeTree});
+});
+
+router.post('/', async (req,res) => {
+  console.log(req.body);
+  const {ownerId, title, description, ingredients, instructions} = req.body;
+  const recipe = new Recipe({
+    path: [],
+    parent: null,
+    ownerId,
+    title,
+    description,
+    ingredients,
+    instructions,
+  });
+  recipe.save()
+  res.send(recipe);
+});
+
+export default router;
