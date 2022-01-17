9ca99712bde14c4ab9f71c943fd32cadf17a0d10
Jamie <lijamie19828@gmail.com>
2022-01-17 12:10:13 -0800

    commentRoutes no longer hard coded

--- server/routes/commentRoutes.ts
+++ server/routes/commentRoutes.ts
@@ -3,18 +3,20 @@ import Comment from '../models/comment'
 
 const router = express.Router();
 
-router.get('/', async (req,res) => {
-  const comments = await Comment.find( {recipeId: '2a'});
-  res.send(comments);
+router.get('/all/:recipeId', async (req,res) => {
+  const comments = await Comment.find({recipeId: req.params.recipeId});
+  console.log(comments);
+  res.send('Route Works');
 })
 
 router.post('/create', async (req,res) => {
-  // const comment = new Comment({
-  //   recipeId: req.body.recipeId,
-  //   ownerId: req.body.ownerId,
-  //   content: req.body.content
-  // })
-  console.log(req.body)
+  const comment = new Comment({
+    recipeId: req.body.recipeId,
+    ownerId: req.body.ownerId,
+    content: req.body.content
+  })
+  console.log(comment)
+  comment.save()
   res.send('Route Works')
 })
 
