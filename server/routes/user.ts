38bff7a53ebdde0afab9a5e6d4338e642d963a20
Jamie <lijamie19828@gmail.com>
2022-01-17 13:38:47 -0800

    refactor

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -4,8 +4,7 @@ import User from '../models/user'
 const router = express.Router();
 
 router.get('/:id', async (req,res) => {
-  const user = await User.find( {_id: '1a'});
+  const user = await User.findOne( {_id: req.params.id});
   res.send(user);
 })
-
 export default router;
