54338609a794a65fb0401d51adeb5b6281a0cf4e
Ivan <ivanxintang@gmail.com>
2022-01-17 20:23:40 -0800

    Change file types to jsx and render Navbar in App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import Dashboard from './pages/Dashboard';
 import Recipe from './pages/Recipe';
 import Versions from './pages/Versions';
 import Navbar from './components/Navbar';
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path={ROUTES.HOME} component={Home} exact />
         <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
