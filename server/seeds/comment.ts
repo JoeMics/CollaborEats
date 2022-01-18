f3c98f2189bc8993581e3b1a84245a4971095dff
Jamie <lijamie19828@gmail.com>
2022-01-17 16:59:41 -0800

    refactor seeds

--- server/seeds/comment.ts
+++ server/seeds/comment.ts
@@ -1,14 +1,14 @@
 export default [
   {
-    _id: '1a',
-    recipeId: '2a',
-    ownerId: '2a',
+    _id: '61e607f0311d699fd35f509e',
+    recipeId: '61e608607f04825b4c4cd517',
+    ownerId: '61e608607f04825b4c4cd517',
     content: 'This is too much pepper for little old me.',
   },
   {
-    _id: '2a',
-    recipeId: '2a',
-    ownerId: '2a',
+    _id: '61e608607f04825b4c4cd517',
+    recipeId: '61e608607f04825b4c4cd517',
+    ownerId: '61e608607f04825b4c4cd517',
     content: 'This is too little pepper for little old me.',
   },
 ];
