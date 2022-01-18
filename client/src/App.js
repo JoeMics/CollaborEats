37a7d0a27e9fb7f8ee5d2d6f1541f5775b6cec0b
Ivan <ivanxintang@gmail.com>
2022-01-17 19:59:01 -0800

    Add React Router and setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import './styles/tailwind.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Recipe from './pages/Recipe';
+import Versions from './pages/Versions';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Mom</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path={ROUTES.HOME} component={Home} exact />
+        <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+        <Route path={ROUTES.RECIPE} component={Recipe} exact />
+        <Route path={ROUTES.VERSIONS} component={Versions} exact />
+      </Switch>
+    </Router>
   );
 }
 
