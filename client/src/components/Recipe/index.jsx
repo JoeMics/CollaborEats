a5b83507343421a8b2a659b964b5ec08e904ccdf
Jamie <lijamie19828@gmail.com>
2022-01-18 14:39:22 -0800

    redid structure as per Joseph guidance

--- client/src/components/Recipe/index.jsx
+++ client/src/components/Recipe/index.jsx
@@ -4,9 +4,16 @@ import Instructions from './Instructions';
 
 export default function RecipeComponent(props) {
   return (
-    <div className="flex justify-center bg-green-500">
-      <Ingredients />
-      <Instructions />
+    <div className="flex-row mx-auto container bg-green-500 pt-16">
+      <div className="flex content-start space-x-6">
+        <h3>This is a very long recipe title</h3>
+        <button>Fork</button>
+        <button>Other Forks</button>
+      </div>
+      <div className="flex container bg-yellow-500">
+        <Ingredients />
+        <Instructions />
+      </div>
     </div>
   );
 }
