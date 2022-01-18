0930c988812894e09acc9b59a441fa3cccbe036d
Joseph Micla <josephmicla@gmail.com>
2022-01-18 14:19:13 -0800

    creates comment section skeleton

--- client/src/Comments/Form.jsx
+++ client/src/Comments/Form.jsx
@@ -2,8 +2,13 @@ const Form = () => {
   return (
     <section>
       <form action="" method="post">
-        <input className="border-2" type="text" />
-        <button type="submit">Submit</button>
+        <input className="border-2 focus:border-blue-400" type="text" />
+        <button
+          className=" inline-flex items-center px-4 py-2 bg-blue-300 rounded text-white"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </section>
   );
--- client/src/Comments/index.jsx
+++ client/src/Comments/index.jsx
@@ -3,9 +3,18 @@ import Form from './Form';
 
 const Comments = () => {
   return (
-    <section className="container mt-10 mx-auto">
+    <section className="container mt-10 mx-auto border-4">
       <h2 className="text-4xl">Comments</h2>
       <Form />
+      {/* comments to mapped over */}
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
       <Comment />
     </section>
   );
