de9ba043790a6c57070add8fd86241bb790630c6
Jamie <lijamie19828@gmail.com>
2022-01-18 14:50:34 -0800

    fix structure

--- client/src/components/Recipe/index.jsx
+++ client/src/components/Recipe/index.jsx
@@ -4,8 +4,8 @@ import Instructions from './Instructions';
 
 export default function RecipeComponent(props) {
   return (
-    <div className="flex-row mx-auto container bg-green-500 pt-16">
-      <div className="flex content-start space-x-6">
+    <div className="flex-row mx-auto container pt-8">
+      <div className="flex content-start space-x-6 bg-green-500">
         <h3>This is a very long recipe title</h3>
         <button>Fork</button>
         <button>Other Forks</button>
