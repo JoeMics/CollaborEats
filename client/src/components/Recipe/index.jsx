1e602a59a632c8675f2cbc01d5f5deaa1013d25e
Jamie <lijamie19828@gmail.com>
2022-01-18 11:28:04 -0800

    component file setup

---
+++ client/src/components/Recipe/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Ingredients from './Ingredients';
+import Instructions from './Instructions';
+
+export default function Recipe(props) {
+  return (
+    
+  );
+}
