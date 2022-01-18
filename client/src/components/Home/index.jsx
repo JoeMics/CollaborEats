fde4ba47de3d7d140a445a2dc832d1fd4725675e
Ivan <ivanxintang@gmail.com>
2022-01-18 12:42:57 -0800

    Implement LandingPage component

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,7 +1,22 @@
+import React from 'react';
 import Hero from './Hero';
+import Jumbotron from './Jumbotron';
+import jumbotronData from '../../data/jumbo-data';
 
 const Home = () => {
-  return ( <Hero />);
+  return ( <>
+  <Hero />
+  {jumbotronData.map((jumbotron) => (
+    <Jumbotron
+     key={jumbotron.id}
+     direction={jumbotron.direction}
+     imgSrc={jumbotron.imgSrc}
+     description={jumbotron.description}
+     background={jumbotron.background}
+     />
+  ))}
+  </>
+  );
 }
  
 export default Home;
