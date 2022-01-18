55bb525a59704c93b2d926d23b4e2ddb9959f95a
Joseph Micla <josephmicla@gmail.com>
2022-01-17 16:38:11 -0800

    create logic for FORKing to /versions

--- server/routes/version.ts
+++ server/routes/version.ts
@@ -2,16 +2,13 @@ import express from 'express';
 import Recipe from '../models/recipe';
 const router = express.Router({ mergeParams: true });
 
-router.get('/:versionId', async (req, res) => {
-  const { versionId } = req.params;
-  const recipe = await Recipe.findOne({ _id: versionId });
-  res.send(recipe);
-});
-
-router.post('/:versionId', async (req, res) => {
+router.post('/', async (req, res) => {
+  const { ownerId, title, description, ingredients, instructions } = req.body;
   // TS does not know recipeId exists
   // @ts-ignore:next-line
-  const { ownerId, title, description, ingredients, instructions, recipeId } = req.body;
+  const { recipeId } = req.params;
+
+  // TODO: OwnerId needs to be retrieved from cookies
   const parentRecipe = await Recipe.findOne({ _id: recipeId });
   const recipe = new Recipe({
     path: [...parentRecipe!.path, recipeId],
@@ -19,11 +16,16 @@ router.post('/:versionId', async (req, res) => {
     ownerId,
     title,
     description,
-    ingredients,
+    ingredients: JSON.parse(ingredients),
     instructions,
   });
-
   recipe.save();
+  res.send(recipe);
+});
+
+router.get('/:versionId', async (req, res) => {
+  const { versionId } = req.params;
+  const recipe = await Recipe.findOne({ _id: versionId });
   res.send(recipe);
 });
 
