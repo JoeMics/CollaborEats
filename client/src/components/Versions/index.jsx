439afcd0c7a7ce045d01f9621a4c1d1f0509de52
Jamie <lijamie19828@gmail.com>
2022-01-18 18:31:52 -0800

    minor refactor of compoenent structure

--- client/src/components/Versions/index.jsx
+++ client/src/components/Versions/index.jsx
@@ -4,11 +4,13 @@ import TreeContainer from './TreeContainer';
 export default function VersionComponent(props) {
   return (
     <div className="flex-row mx-auto container pt-8">
-      <div className="flex content-start space-x-6 bg-green-500">
-        <h3>This is a very long recipe title</h3>
-        <button className="bg-red-500">Original Fork</button>
-        <button className="bg-red-500">Latest Fork</button>
-        <button className="bg-red-500">Most Forks</button>
+      <div className="flex justify-between content-start space-x-6 bg-green-500">
+        <h3 className="pl-2">This is a very long recipe title</h3>
+        <div className="space-x-6 pr-2">
+          <button className="bg-red-500">Original Fork</button>
+          <button className="bg-red-500">Latest Fork</button>
+          <button className="bg-red-500">Most Forks</button>
+        </div>
       </div>
       <div className="flex-row mx-auto container pt-6">
         <TreeContainer />
