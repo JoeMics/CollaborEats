ec3bfc5d8a76f27916b0f3cf05406d5c925dbed5
Ivan <ivanxintang@gmail.com>
2022-01-18 17:43:13 -0800

    Refactor class to className for Hero component

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button className="flex items-center px-4 py-2 bg-blue-300 rounded text-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -26,7 +26,7 @@ const Hero = () => {
           Fork it Now
         </button>
       </div>
-      <div className='w-1/2 min-h-full'>
+      <div className="w-1/2 min-h-full">
         <img className="object-fit" src="https://source.unsplash.com/random/600x600" alt="hero" />
       </div>
     </div>
