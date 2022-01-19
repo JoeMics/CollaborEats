6547a237f1eb12d1780c9c55b8b399c4dc24c47d
Jamie <lijamie19828@gmail.com>
2022-01-18 18:25:04 -0800

    add missing feature according to wireframe

--- client/src/components/Versions/index.jsx
+++ client/src/components/Versions/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import TreeContainer from './TreeBox';
+import TreeContainer from './TreeContainer';
 
 export default function VersionComponent(props) {
   return (
     <div className="flex-row mx-auto container pt-8">
       <div className="flex content-start space-x-6 bg-green-500">
+        <h3>This is a very long recipe title</h3>
         <button className="bg-red-500">Original Fork</button>
         <button className="bg-red-500">Latest Fork</button>
         <button className="bg-red-500">Most Forks</button>
