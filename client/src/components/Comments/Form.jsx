0ae78def29fb05e2850252d38f215608bfce5954
Joseph Micla <josephmicla@gmail.com>
2022-01-19 13:33:54 -0800

    refactor comment form to controlled component

--- client/src/components/Comments/Form.jsx
+++ client/src/components/Comments/Form.jsx
@@ -1,11 +1,39 @@
-const Form = () => {
+import { useState } from 'react';
+import { addComment } from '../../services/api';
+
+const Form = ({ userId, recipeId }) => {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = async (e) => {
+    // TODO: add validations/sanitizations
+    e.preventDefault();
+
+    try {
+      await addComment(userId, recipeId, input);
+    } catch (error) {
+      console.log(error);
+    }
+    setInput('');
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
     <section>
-      <form action="" method="post">
-        <input className="border-2 focus:border-blue-400" type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="border-2 focus:border-blue-400"
+          name="comment"
+          value={input}
+          onChange={handleChange}
+          type="text"
+        />
         <button
           className=" inline-flex items-center px-4 py-2 bg-blue-300 rounded text-white"
           type="submit"
+          value="Submit"
         >
           Submit
         </button>
--- client/src/components/Comments/index.jsx
+++ client/src/components/Comments/index.jsx
@@ -1,11 +1,15 @@
 import Comment from './Comment';
 import Form from './Form';
 
+// DEBUGGING
+const RECIPEID = '61e608607f04825b4c4cd517';
+const USERID = '61e608607f04825b4c4cd517';
+
 const Comments = () => {
   return (
     <section className="container mt-10 mx-auto border-4">
       <h2 className="text-4xl">Comments</h2>
-      <Form />
+      <Form userId={USERID} recipeId={RECIPEID} />
       {/* comments to mapped over */}
       <Comment />
       <Comment />
