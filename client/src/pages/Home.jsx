7a5203ee3891a6769ede50a317056821c64d0c85
Ivan <ivanxintang@gmail.com>
2022-01-18 17:44:42 -0800

    Setup example of api calls in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import LandingPage from '../components/Home';
 import Footer from '../components/Footer';
+import { fetchData } from '../services/api';
 
 const Home = () => {
+  const [data, setData] = useState(null);
   useEffect(() => {
     document.title = 'Home - CollaborEats';
-  },[])
-  
-  return ( 
-  <div>
-    <LandingPage />
-    <Footer />
-  </div> );
-}
- 
+  }, []);
+
+  //example
+  useEffect(() => {
+    async function getAllData() {
+      const dbData = await fetchData();
+      console.log('dbData: ', dbData.data);
+      setData(dbData.data);
+      console.log(data);
+    }
+    getAllData();
+  }, []);
+
+  return (
+    <div>
+      <LandingPage />
+      <Footer />
+    </div>
+  );
+};
+
 export default Home;
