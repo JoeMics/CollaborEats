bd614e205ef44ffac3c0d2af9faa673775ccdf26
Joseph Micla <josephmicla@gmail.com>
2022-01-19 13:33:12 -0800

    create api POST request to create comments

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,12 @@
 import axios from '../lib/api';
 
 export async function fetchData() {
-  const response = await axios.get('/');
-  return response;
+  return await axios.get('/');
+}
+
+export async function addComment(ownerId, recipeId, content) {
+  return await axios.post(`/recipes/${recipeId}/comments`, {
+    ownerId,
+    content,
+  });
 }
