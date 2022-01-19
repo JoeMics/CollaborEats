7e0844bb2d8e25069b617043b97f73227efda01b
Ivan <ivanxintang@gmail.com>
2022-01-19 15:45:31 -0800

    Change Tree.jsx to use tree formatter and render tree from our db

--- client/src/components/Versions/Tree.jsx
+++ client/src/components/Versions/Tree.jsx
@@ -1,39 +1,40 @@
-import { render } from '@testing-library/react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tree from 'react-d3-tree';
-
-const testData = [
-  {
-    name: '1',
-    children: [
-      {
-        name: '2',
-        children: [
-          {
-            name: '3',
-          },
-        ],
-      },
-      {
-        name: '2',
-        children: [
-          {
-            name: '3',
-          },
-          {
-            name: '3',
-          },
-        ],
-      },
-    ],
-  },
-];
+import treeFormatter from '../../helpers/treeFormatter';
+import { fetchData } from '../../services/api';
 
 export default function OrgChartTree() {
+  const [treeData, setTreeData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getAllData() {
+      const dbData = await fetchData();
+
+      const root = {
+        name: dbData.data.recipe.title,
+        id: dbData.data.recipe._id,
+        children: [],
+      };
+
+      const treeArray = dbData.data.recipeTree;
+      const formattedAnswer = treeFormatter(root, treeArray);
+      setTreeData(formattedAnswer);
+      setLoading(false);
+    }
+    setLoading(true);
+    getAllData();
+  }, []);
+
   return (
     // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
-    <div id="treeWrapper" style={{ width: '80vw', height: '75vh' }}>
-      <Tree data={testData} orientation={'horizontal'} />
-    </div>
+    <>
+      {loading && <div>Loading!!!</div>};
+      {!loading && (
+        <div id="treeWrapper" style={{ width: '80vw', height: '75vh' }}>
+          <Tree data={treeData} orientation={'vertical'} />
+        </div>
+      )}
+    </>
   );
 }
