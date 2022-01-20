0ad3269cec631d921d700fa8ab83a7d9b0acf4fc
Ivan <ivanxintang@gmail.com>
2022-01-20 13:49:34 -0800

    Add new routes into route constants

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -2,3 +2,4 @@ export const HOME = '/';
 export const DASHBOARD = '/dashboard';
 export const RECIPE = '/recipe';
 export const VERSIONS = '/versions';
+export const RECIPEPAGE = '/recipes/originals';
