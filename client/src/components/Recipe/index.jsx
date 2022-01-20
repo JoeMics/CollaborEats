08387b57097823fb69dfe42526bed9cdcb55db78
Ivan <ivanxintang@gmail.com>
2022-01-19 16:57:01 -0800

    Implement data fetch, setup loading, pass down props to correct components

--- client/src/components/Recipe/index.jsx
+++ client/src/components/Recipe/index.jsx
@@ -3,41 +3,37 @@ import Ingredients from './Ingredients';
 import Instructions from './Instructions';
 import CreateFormComponent from './CreateForm';
 import EditFormComponent from './EditForm';
-import { getRecipeData } from '../../services/api';
+import { getRecipe } from '../../services/api';
 
 export default function RecipeComponent(props) {
+  const [recipe, setRecipe] = useState(true);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function getAllData() {
-      const dbData = getRecipeData();
-
-      const root = {
-        name: dbData.data.recipe.title,
-        id: dbData.data.recipe._id,
-        children: [],
-      };
-      setTreeData(formattedAnswer);
+    async function getRecipeData() {
+      setLoading(true);
+      const dbData = await getRecipe();
       setLoading(false);
+      console.log('edit form data:, ', dbData.data.recipe);
+      setRecipe(dbData.data.recipe);
     }
-    setLoading(true);
     getRecipeData();
   }, []);
 
+  // const handleCreate = () => {}
+  // const handleFork = () => {}
   return (
     <div className="flex-row mx-auto container pt-8">
-      {/* <div className="flex content-start space-x-6 bg-green-500">
-        <h3>This is a very long recipe title</h3>
+      <div className="flex content-start space-x-6 bg-green-500">
+        <h3>{recipe.title}</h3>
         <button>Fork</button>
         <button>Other Forks</button>
       </div>
       <div className="flex container bg-yellow-500">
-        <Ingredients />
-        <Instructions />
-      </div> */}
-      <EditFormComponent />
+        <Ingredients ingredients={recipe.ingredients} />
+        <Instructions instructions={recipe.instructions} description={recipe.description} />
+      </div>
+      {/* <EditFormComponent /> */}
     </div>
   );
 }
-
-//61e607f0311d699fd35f509e
