06f292ded1f67ae8428cf790a4b36bbd542ca92b
Joseph Micla <josephmicla@gmail.com>
2022-01-19 16:56:22 -0800

    adds break-words to comments, centered with flex

--- client/src/components/Comments/Comment.jsx
+++ client/src/components/Comments/Comment.jsx
@@ -1,10 +1,10 @@
 const Comment = ({ name, content, createdAt }) => {
   return (
-    <article className="px-5 py-2 flex">
+    <article className="px-5 py-2 flex justify-center">
       <div>
         <img src="https://i.imgur.com/2WZtOD6.png" alt="" />
       </div>
-      <div className="bg-stone-300 mx-3 p-4 rounded-lg">
+      <div className="bg-stone-300 mx-3 p-4 break-words rounded-lg w-4/5">
         <h4 className="text-m">{name}</h4>
         <p className="text-xs h-auto">{content}</p>
         <span className="text-xs">created on: {createdAt}</span>
