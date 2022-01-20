131144a4008d2e30ffbd50292e81230eededbc48
Ivan <ivanxintang@gmail.com>
2022-01-19 21:08:21 -0800

    Implement adding and removing ingredients

--- client/src/components/Recipe/List.jsx
+++ client/src/components/Recipe/List.jsx
@@ -1,27 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ListComponent from './ListComponent';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function List(props) {
-  const Input = () => {
-    return <ListComponent />;
+export default function List({ ingredients, edit }) {
+  const [ingredientList, setIngredientList] = useState([]);
+
+  useEffect(() => {
+    setIngredientList([...ingredients]);
+  }, [ingredients]);
+
+  //ADD NEW INGREDIENT we have no id. we will use state then
+  //REFRESHED THE PAGE. will have id.
+  // ADD TO LIST
+
+  const onAddBtnClick = () => {
+    const ingredient = {
+      ingredient: null,
+      amount: null,
+      unitOfMeasure: null,
+    };
+
+    setIngredientList([...ingredientList, ingredient]);
   };
 
-  const [inputList, setInputList] = useState([]);
-
-  const onAddBtnClick = (e) => {
-    e.preventDefault();
-    setInputList(inputList.concat(<Input key={inputList.length} />));
+  const onRemoveBtnClick = (uuid) => {
+    const filteredList = ingredientList.filter((item) => {
+      return item.listId !== uuid;
+    });
+    setIngredientList(filteredList);
   };
-
-  // const onRemoveBtnClick = (e) => {
-  //   e.preventDefault();
-  //   const updatedInput = inputList.filter( )
-  // }
 
   return (
     <div>
-      {inputList}
-      <button onClick={onAddBtnClick}>Add an ingredient</button>
+      {edit &&
+        ingredientList.map((ingredient) => {
+          const uuid = uuidv4();
+          ingredient['listId'] = uuid;
+          return (
+            <ListComponent key={uuid} item={ingredient} remove={() => onRemoveBtnClick(uuid)} />
+          );
+        })}
+      <button type="button" onClick={onAddBtnClick}>
+        Add an ingredient
+      </button>
     </div>
   );
 }
