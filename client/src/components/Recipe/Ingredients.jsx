454953e0d11f05ca6d7d6c1b95ce75328668eea4
Ivan <ivanxintang@gmail.com>
2022-01-19 16:56:18 -0800

    Render ingredients info from db

--- client/src/components/Recipe/Ingredients.jsx
+++ client/src/components/Recipe/Ingredients.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 
-export default function Ingredients(props) {
+export default function Ingredients({ ingredients }) {
   //ingredientsList = getIngredients()
   return (
     <div className="w-1/6 bg-blue-500">
       <h3 className="mt-4 ml-4">Ingredients</h3>
       <ul className="m-4">
-        {/* ingredientsList.map {(ingredient) => {
+        {ingredients.map((ingredient) => (
           <li key={ingredient._id}>
-          {ingredient.ingredient}: {ingredient.amount} {ingredient.unitOfMeasure? ingredient.unitOfMeasure : null}
+            {ingredient.ingredient}: {ingredient.amount}{' '}
+            {ingredient.unitOfMeasure ? ingredient.unitOfMeasure : null}
           </li>
-        }} */}
-
-        <li> Flour: 2 Cups</li>
-        <li> Flour: 2 Cups</li>
-        <li> Flour: 2 Cups</li>
-        <li> Flour: 2 Cups</li>
-        <li> Flour: 2 Cups</li>
-        <li> Flour: 2 Cups</li>
-        <li> Flour: 2 Cups</li>
+        ))}
       </ul>
     </div>
   );
