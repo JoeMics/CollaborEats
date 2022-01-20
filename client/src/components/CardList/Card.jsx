3868022b646bf5dc67f2337c8b4c32e54eb218a2
Ivan <ivanxintang@gmail.com>
2022-01-20 15:04:24 -0800

    Make links to individual recipes in card list

--- client/src/components/CardList/Card.jsx
+++ client/src/components/CardList/Card.jsx
@@ -1,7 +1,11 @@
-const Card = ({ title, description }) => {
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+const Card = ({ title, description, id }) => {
   return (
     <article className="border-2 p-5 mx-14 my-4 w-96 rounded">
-      <img src="https://source.unsplash.com/random/400x400" alt="" />
+      <Link to={`${ROUTES.RECIPE}/${id}`}>
+        <img src="https://source.unsplash.com/random/400x400" alt="" />
+      </Link>
       <h3 className="text-2xl py-2">{title}</h3>
       <p className="text-m py-2">{description}</p>
     </article>
--- client/src/components/CardList/index.jsx
+++ client/src/components/CardList/index.jsx
@@ -1,12 +1,16 @@
 import Card from './Card';
-
 // Must be a "grid" layout
 const CardList = ({ recipes }) => {
   return (
     <section className="container flex flex-wrap justify-around">
       {recipes &&
         recipes.map((recipe) => (
-          <Card key={recipe._id} title={recipe.title} description={recipe.description} />
+          <Card
+            key={recipe._id}
+            title={recipe.title}
+            description={recipe.description}
+            id={recipe._id}
+          />
         ))}
     </section>
   );
