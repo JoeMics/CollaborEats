14e26ca17173fe8c0031466da772b764dbb4632c
Ivan <ivanxintang@gmail.com>
2022-01-19 17:43:10 -0800

    Add toggle state and conditional rendering for edit form

--- client/src/components/Recipe/index.jsx
+++ client/src/components/Recipe/index.jsx
@@ -8,6 +8,7 @@ import { getRecipe } from '../../services/api';
 export default function RecipeComponent(props) {
   const [recipe, setRecipe] = useState(true);
   const [loading, setLoading] = useState(true);
+  const [toggleForm, setToggleForm] = useState(false);
 
   useEffect(() => {
     async function getRecipeData() {
@@ -23,17 +24,32 @@ export default function RecipeComponent(props) {
   // const handleCreate = () => {}
   // const handleFork = () => {}
   return (
-    <div className="flex-row mx-auto container pt-8">
-      <div className="flex content-start space-x-6 bg-green-500">
-        <h3>{recipe.title}</h3>
-        <button>Fork</button>
-        <button>Other Forks</button>
-      </div>
-      <div className="flex container bg-yellow-500">
-        <Ingredients ingredients={recipe.ingredients} />
-        <Instructions instructions={recipe.instructions} description={recipe.description} />
-      </div>
-      {/* <EditFormComponent /> */}
-    </div>
+    <>
+      {loading && <div>Loading!!!</div>}
+      {!loading && (
+        <div className="flex-row mx-auto container pt-8">
+          {!toggleForm ? (
+            <>
+              <div className="flex content-start space-x-6 bg-green-500">
+                <h3>{recipe.title}</h3>
+                <button onClick={() => setToggleForm(!toggleForm)}>Fork</button>
+                <button>Other Forks</button>
+              </div>
+              <div className="flex container bg-yellow-500">
+                <Ingredients ingredients={recipe.ingredients} />
+                <Instructions instructions={recipe.instructions} description={recipe.description} />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex content-start space-x-6 bg-green-500">
+                <button onClick={() => setToggleForm(!toggleForm)}>Back</button>
+              </div>
+              <EditFormComponent recipe={recipe} />
+            </>
+          )}
+        </div>
+      )}
+    </>
   );
 }
