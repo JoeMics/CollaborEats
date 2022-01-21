ae7cbc17d2b3b5d01f25539f3c853b07529b2d94
Ivan <ivanxintang@gmail.com>
2022-01-21 12:45:24 -0800

    Connect frontend to image uploading backend

--- client/src/pages/ImageUpload.jsx
+++ client/src/pages/ImageUpload.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const ImageUpload = () => {
-  return <div>I am image uploading</div>;
+  return (
+    <div className="container">
+      <div className="flex-row">
+        <div className="m-auto">
+          <h1 className="font-bold text-xl text-center my-4">Upload Files</h1>
+          <form action="http://localhost:8080/upload" method="POST" enctype="multipart/form-data">
+            <div className="custom-file mb-3">
+              <input type="file" name="file" id="file" class="custom-file-input" />
+              <label htmlFor="for" className="custom-file-label">
+                Choose File
+              </label>
+            </div>
+            <input type="submit" value="Submit" className="px-3 px-3 bg-blue-400" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageUpload;
