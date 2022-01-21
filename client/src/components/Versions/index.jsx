51ebc380dc279dd8024354c220ebdcd7570a4d0e
Jamie <lijamie19828@gmail.com>
2022-01-21 14:00:17 -0800

    Link Original Fork button

--- client/src/components/Versions/index.jsx
+++ client/src/components/Versions/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import TreeContainer from './TreeContainer';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
 export default function VersionComponent() {
   const { id } = useParams();
   return (
@@ -9,10 +12,10 @@ export default function VersionComponent() {
         <h2 className="text-5xl font-serif">Recipe Tree</h2>
         <div className="flex">
           <button className="block items-center mx-2 px-4 py-2 bg-blue-300 rounded text-white">
-            Original Fork
+            <Link to={`${ROUTES.RECIPE}/${id}`}>Original Fork</Link>
           </button>
           <button className="block items-center mx-2 px-4 py-2 bg-blue-300 rounded text-white">
-            Latest Fork
+            Most Recent Fork
           </button>
           <button className="block items-center mx-2 px-4 py-2 bg-blue-300 rounded text-white">
             Most Forks
