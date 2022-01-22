e0ba7618d127eef717cd167c63463f1a5e4b0fb9
Jamie <lijamie19828@gmail.com>
2022-01-22 14:29:17 -0800

    try catch versions post

--- server/routes/version.ts
+++ server/routes/version.ts
@@ -3,26 +3,30 @@ import Recipe from '../models/recipe';
 const router = express.Router({ mergeParams: true });
 
 router.post('/', async (req, res) => {
-  const { ownerId, title, description, ingredients, instructions } = req.body;
-  console.log(title);
-  // TS does not know recipeId exists
-  // @ts-ignore:next-line
-  const { recipeId } = req.params;
+  try {
+    const { ownerId, title, description, ingredients, instructions } = req.body;
+    console.log(title);
+    // TS does not know recipeId exists
+    // @ts-ignore:next-line
+    const { recipeId } = req.params;
 
-  // TODO: OwnerId needs to be retrieved from cookies
-  const parentRecipe = await Recipe.findOne({ _id: recipeId });
+    // TODO: OwnerId needs to be retrieved from cookies
+    const parentRecipe = await Recipe.findOne({ _id: recipeId });
 
-  const recipe = new Recipe({
-    path: [...parentRecipe!.path, recipeId],
-    parent: recipeId,
-    ownerId,
-    title,
-    description,
-    ingredients: ingredients,
-    instructions,
-  });
-  recipe.save();
-  res.send(recipe);
+    const recipe = new Recipe({
+      path: [...parentRecipe!.path, recipeId],
+      parent: recipeId,
+      ownerId,
+      title,
+      description,
+      ingredients: ingredients,
+      instructions,
+    });
+    await recipe.save();
+    res.send(recipe);
+  } catch (err) {
+    res.status(500).json({ err: err });
+  }
 });
 
 router.get('/:versionId', async (req, res) => {
