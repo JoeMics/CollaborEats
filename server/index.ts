7381dc9f4bd0962993d9caae02dc9b3fdd91ad43
Ivan <ivanxintang@gmail.com>
2022-01-21 18:38:48 -0800

    Create endpoint for getting all images

--- server/index.ts
+++ server/index.ts
@@ -86,26 +86,27 @@ const storage = new GridFsStorage({
 });
 const upload = multer({ storage });
 
-// app.get('/upload', (req, res) => {
-//   gfs.files.find().toArray((err: any, files: any) => {
-//     if (!files || files.length === 0) {
-//       return res.status(404).json({
-//         err: 'No files exist',
-//       });
-//     } else {
-//       // @ts-ignore:next-line
-//       files.map((file) => {
-//         if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-//           file.isImage = true;
-//         } else {
-//           file.isImage = false;
-//         }
-//       });
-//       res.send({ files: files });
-//     }
-//   });
-// });
+app.get('/upload', (req, res) => {
+  // @ts-ignore:next-line
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      res.send({ files: false });
+    } else {
+      // @ts-ignore:next-line
+      files.map((file) => {
+        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res.send({ files: files });
+    }
+  });
+});
 app.post('/upload', upload.single('file'), (req, res) => {
+  console.log(req.file);
   res.json({ file: req.file });
 });
 
