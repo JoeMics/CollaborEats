043ebf100fa11e0b3837a99f67415c6e83a93626
Jamie <lijamie19828@gmail.com>
2022-01-21 20:55:13 -0800

    try catch recipe.ts

--- server/routes/recipe.ts
+++ server/routes/recipe.ts
@@ -8,56 +8,96 @@ router.use('/:recipeId/versions/', versionsRoute);
 router.use('/:recipeId/comments/', commentsRoute);
 
 router.get('/', async (req, res) => {
-  const recipes = await Recipe.find({ parent: null });
-  res.send(recipes);
+  try {
+    const recipes = await Recipe.find({ parent: null });
+    if (!recipes) {
+      throw new Error('Recipe not found');
+    }
+    res.send(recipes);
+  } catch (err) {
+    return res.send(err);
+  }
 });
 
 router.get('/:id', async (req, res) => {
-  const recipe = await Recipe.findOne({ _id: req.params.id });
-  const recipeTree = await Recipe.find({ path: req.params.id });
-  res.send({ recipe, recipeTree });
+  try {
+    const recipe = await Recipe.findOne({ _id: req.params.id });
+    const recipeTree = await Recipe.find({ path: req.params.id });
+    if (!recipe) {
+      throw new Error('Recipe not found');
+    }
+    res.send({ recipe, recipeTree });
+  } catch (err) {
+    return res.send(err);
+  }
 });
 
 router.get('/user/:ownerId', async (req, res) => {
-  const recipes = await Recipe.find({ ownerId: req.params.ownerId });
-  res.send(recipes);
+  try {
+    const recipes = await Recipe.find({ ownerId: req.params.ownerId });
+    if (!recipes) {
+      throw new Error('Recipes not found');
+    }
+    res.send(recipes);
+  } catch (err) {
+    return res.send(err);
+  }
 });
 
 router.get('/:id/recent', async (req, res) => {
-  const recipe = await Recipe.findOne({ _id: req.params.id });
-  const recipeTree = await Recipe.find({ path: req.params.id }).sort({ createdAt: 'desc' });
-  console.log(recipe);
+  try {
+    const recipe = await Recipe.findOne({ _id: req.params.id });
+    const recipeTree = await Recipe.find({ path: req.params.id }).sort({ createdAt: 'desc' });
+    if (!recipe && !recipeTree) {
+      throw new Error('Recipes not found');
+    }
 
-  if (recipeTree.length > 0) {
-    res.send(recipeTree[0]);
-  } else {
-    res.send(recipe);
+    if (recipeTree.length > 0) {
+      res.send(recipeTree[0]);
+    } else {
+      res.send(recipe);
+    }
+  } catch (err) {
+    return res.send(err);
   }
 });
 
 router.get('/:id/mostForked', async (req, res) => {
-  const recipe = await Recipe.findOne({ _id: req.params.id });
-  const recipeTree = await Recipe.find({ path: req.params.id });
-  if (recipeTree.length > 0) {
-    res.send(recipeTree);
-  } else {
-    res.send(recipe);
+  try {
+    const recipe = await Recipe.findOne({ _id: req.params.id });
+    const recipeTree = await Recipe.find({ path: req.params.id });
+
+    if (!recipe || !recipeTree) {
+      throw new Error('Recipe not found');
+    }
+
+    if (recipeTree.length > 0) {
+      res.send(recipeTree);
+    } else {
+      res.send(recipe);
+    }
+  } catch (err) {
+    return res.send(err);
   }
 });
 
 router.post('/', async (req, res) => {
-  const { ownerId, title, description, ingredients, instructions } = req.body;
-  const recipe = new Recipe({
-    path: [],
-    parent: null,
-    ownerId,
-    title,
-    description,
-    ingredients,
-    instructions,
-  });
-  recipe.save();
-  res.send(recipe);
+  try {
+    const { ownerId, title, description, ingredients, instructions } = req.body;
+    const recipe = new Recipe({
+      path: [],
+      parent: null,
+      ownerId,
+      title,
+      description,
+      ingredients,
+      instructions,
+    });
+    await recipe.save();
+    res.send(recipe);
+  } catch (err) {
+    res.status(500).json({ err: err });
+  }
 });
 
 export default router;
