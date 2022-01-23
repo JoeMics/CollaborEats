e9312ffb26e6ac61eda96543e9ce2174176817e6
Joseph Micla <josephmicla@gmail.com>
2022-01-23 12:56:44 -0800

    adds animation to cards and tree cards

--- client/src/components/CardList/Card.jsx
+++ client/src/components/CardList/Card.jsx
@@ -3,22 +3,22 @@ import * as ROUTES from '../../constants/routes';
 const Card = ({ title, description, id, photo, ownerId }) => {
   return (
     <div className="basis-1/3">
-      <article className="flex-row border-2 p-5 my-4 w-96 rounded">
-        <Link to={`${ROUTES.RECIPE}/${id}`}>
+      <Link to={`${ROUTES.RECIPE}/${id}`}>
+        <article className="flex-row border-2 p-5 my-4 w-96 rounded shadow-slate-500 shadow-md hover:scale-105 group-hover:transition-all duration-300">
           <img
-            className="h-96 w-96"
+            className="h-96 w-96 rounded-lg object-cover"
             src={
               photo
                 ? `http://localhost:8080/image/${photo}`
-                : 'https://source.unsplash.com/random/400x400'
+                : 'https://source.unsplash.com/random/?food'
             }
             alt="Recipe Card"
           />
-        </Link>
-        <h3 className="text-2xl py-2 truncate">{title}</h3>
-        <p className="text-m py-2 truncate">{description}</p>
-        <p className="text-m py-2 truncate">{ownerId}</p>
-      </article>
+          <h3 className="text-2xl underline py-2 mx-2 truncate">{title}</h3>
+          <p className="text-m pb-2 mx-2 truncate">by {ownerId}</p>
+          <p className="text-lg py-3 mx-2 truncate">{description}</p>
+        </article>
+      </Link>
     </div>
   );
 };
