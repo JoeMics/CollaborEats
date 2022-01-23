fbebc8c74276ff8dcc0965a9e4166318c1d8116e
Ivan <ivanxintang@gmail.com>
2022-01-23 12:35:53 -0800

    Create base modal form styles

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -2,9 +2,17 @@ import RecipeForm from '../components/RecipeForm';
 
 const Create = () => {
   return (
-    <div className="container w-3/5">
-      <RecipeForm title="Create a NEW Recipe!" />
-    </div>
+    <>
+      {/* fixed inset-0 z-50 if you want modal */}
+      <div className="justify-center items-center flex fixed inset-0 z-50 overflow-x-hidden overflow-y-autooutline-none focus:outline-none">
+        <div className="relative w-auto mx-auto my-8 mb-8 max-w-3xl">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none ">
+            <RecipeForm title="Create a NEW Recipe!" />
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
   );
 };
 
