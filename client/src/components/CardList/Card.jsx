7b576dd66314633b2d0ee3a706e98ef6c4cd1bcc
Ivan <ivanxintang@gmail.com>
2022-01-22 18:43:38 -0800

    Truncate text on cards

--- client/src/components/CardList/Card.jsx
+++ client/src/components/CardList/Card.jsx
@@ -6,16 +6,17 @@ const Card = ({ title, description, id, photo }) => {
       <article className="flex-row border-2 p-5 my-4 w-96 rounded">
         <Link to={`${ROUTES.RECIPE}/${id}`}>
           <img
+            className="h-96 w-96"
             src={
               photo
                 ? `http://localhost:8080/image/${photo}`
                 : 'https://source.unsplash.com/random/400x400'
             }
-            alt=""
+            alt="Recipe Card"
           />
         </Link>
-        <h3 className="text-2xl py-2">{title}</h3>
-        <p className="text-m py-2">{description}</p>
+        <h3 className="text-2xl py-2 truncate">{title}</h3>
+        <p className="text-m py-2 truncate">{description}</p>
       </article>
     </div>
   );
