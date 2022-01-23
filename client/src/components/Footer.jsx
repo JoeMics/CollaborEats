3f4e6a70b72be5b03f057ea863140092e67bfd2b
Ivan <ivanxintang@gmail.com>
2022-01-23 12:35:14 -0800

    Change padding of footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return ( 
-    <footer className="py-4 w-full bg-gray-700 text-white text-center">
+  return (
+    <footer className="py-8 w-full bg-gray-700 text-white text-center">
       Copyright © 2022 | CollaborEats | All Rights Reserved
     </footer>
-   );
-}
- 
+  );
+};
+
 export default Footer;
