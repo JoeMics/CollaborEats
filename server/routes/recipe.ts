f0d5f54ce19b04e5776cc42a555596e9a72ba718
Jamie <lijamie19828@gmail.com>
2022-01-22 21:59:44 -0800

    simple partial search by title, case insensitive

--- server/routes/recipe.ts
+++ server/routes/recipe.ts
@@ -102,3 +102,12 @@ router.post('/', async (req, res) => {
 });
 
 export default router;
+
+router.post('/search', async (req, res) => {
+  console.log(req.body.title);
+  const recipes = await await Recipe.find({
+    $and: [{ title: { $regex: req.body.title, $options: 'i' } }, { parent: null }],
+  });
+  console.log(recipes);
+  res.send(recipes);
+});
