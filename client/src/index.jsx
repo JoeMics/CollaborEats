45ff5892aa4922b4520ff2e3d8d0adc2d558f870
Ivan <ivanxintang@gmail.com>
2022-01-22 18:42:01 -0800

    Import Theme Context to wrap around App

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-
+import { ThemeProvider } from './context/ThemeContext';
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider>
+      <main class="dark:bg-slate-700 bg-white transition-all">
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </main>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
