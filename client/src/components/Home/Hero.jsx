fd60bd96d3d0b37cbfa28026399e4f463dac2315
Joseph Micla <josephmicla@gmail.com>
2022-01-24 15:14:56 -0800

    adds hardcoded cards to landing page

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
                 <p className="mt-4 text-lg text-gray-300 font-semibold tracking-wider">
                   Create Your First Recipe Now
                 </p>
-                <button className="px-4 py-2 mt-4 bg-teal-700 rounded text-white hover:bg-blue-600">
+                <button className="px-4 py-2 mt-4 bg-primary-700 rounded text-white hover:bg-primary-800">
                   <Link to={`${ROUTES.RECIPEPAGE}`} className="flex item-center">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -66,57 +66,55 @@ const Hero = () => {
             y="0"
           >
             <polygon
-              className="text-gray-300 fill-current"
+              className="text-gray-300 dark:text-dark-1000 fill-current"
               points="2560 0 2560 100 0 100"
             ></polygon>
           </svg>
         </div>
       </div>
 
-      <section className="pb-20 bg-gray-300 -mt-48">
+      <section className="pb-20 bg-gray-300 dark:bg-dark-1000 dark:text-neutral-300 -mt-48">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap">
             <div className="relative basis-1/3 pt-12">
-              <article className="py-5 px-12 my-4 w-68 mr-16 rounded-lg bg-white mx-auto">
+              <article className="py-5 px-8 my-4 w-68 mr-16 rounded-lg bg-neutral-200 dark:bg-dark-200 dark:shadow-black dark:shadow-lg shadow-md shadow-dark-50 mx-auto">
                 <img
-                  className="h-44 w-68 rounded-lg object-cover mx-auto"
-                  src="https://source.unsplash.com/random/500x300/?food"
+                  className="h-44 w-full rounded-md object-cover mx-auto object-bottom"
+                  src="https://images.unsplash.com/photo-1551892374-ecf8754cf8b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
                   alt="Recipe Card"
                 />
-                <h3 className="text-2xl underline py-2 mx-2 truncate">Title Title</h3>
-                <p className="text-m pb-2 mx-2 truncate">by SOMEGUY</p>
-                <p className="text-lg py-3 mx-2 truncate">
-                  sladkfjsdklfjs flksjd flksd jflksdj fldkskl
-                </p>
+                <h3 className="text-2xl underline py-2 mx-2 truncate">Spaghetti v1</h3>
+                <p className="text-m pb-2 mx-2 truncate">by Ivan Tang</p>
+                <p className="text-lg py-3 mx-2 truncate">Simple is always best!</p>
               </article>
             </div>
 
             <div className="relative basis-1/3">
-              <article className="py-5 px-12 my-4 w-68 mr-16 rounded-lg bg-white mx-auto">
+              <article className="py-5 px-8 my-4 w-68 mr-12 rounded-lg bg-neutral-200 dark:bg-dark-200 dark:shadow-black dark:shadow-lg shadow-md shadow-dark-50 mx-auto">
                 <img
-                  className="h-48 w-68 rounded-lg object-cover mx-auto"
-                  src="https://source.unsplash.com/random/500x300/?food"
+                  className="h-48 w-full rounded-md object-cover mx-auto "
+                  src="https://images.unsplash.com/photo-1635264685671-739e75e73e0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
                   alt="Recipe Card"
                 />
-                <h3 className="text-2xl underline py-2 mx-2 truncate">Title Title</h3>
-                <p className="text-m pb-2 mx-2 truncate">by SOMEGUY</p>
+                <h3 className="text-2xl underline py-2 mx-2 truncate">Spaghetti v2</h3>
+                <p className="text-m pb-2 mx-2 truncate">by Jamie Li</p>
                 <p className="text-lg py-3 mx-2 truncate">
-                  sladkfjsdklfjs flksjd flksd jflksdj fldkskl
+                  When it comes to pasta, more cheese is better!
                 </p>
               </article>
             </div>
 
             <div className="relative basis-1/3 pt-6">
-              <article className="py-5 px-12 my-4 w-68 mr-16 rounded-lg bg-white mx-auto">
+              <article className="py-5 px-8 my-4 w-68 mr-16 rounded-lg bg-neutral-200 dark:bg-dark-200 dark:shadow-black dark:shadow-lg shadow-md shadow-dark-50 mx-auto">
                 <img
-                  className="h-48 w-68 rounded-lg object-cover mx-auto"
-                  src="https://source.unsplash.com/random/500x300/?food"
+                  className="h-48 w-full rounded-md mx-auto object-cover object-center"
+                  src="https://images.unsplash.com/photo-1626844131082-256783844137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
                   alt="Recipe Card"
                 />
-                <h3 className="text-2xl underline py-2 mx-2 truncate">Title Title</h3>
-                <p className="text-m pb-2 mx-2 truncate">by SOMEGUY</p>
+                <h3 className="text-2xl underline py-2 mx-2 truncate">Spaghetti v3</h3>
+                <p className="text-m pb-2 mx-2 truncate">by Joseph Micla</p>
                 <p className="text-lg py-3 mx-2 truncate">
-                  sladkfjsdklfjs flksjd flksd jflksdj fldkskl
+                  Good pasta should cost at least $30.00!
                 </p>
               </article>
             </div>
