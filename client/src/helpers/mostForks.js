942de14234fbc1777dafa0ca7cbbee2ddddbd032
Jamie <lijamie19828@gmail.com>
2022-01-24 19:36:40 -0800

    add extra validation of input to check if parent or child recipe

--- client/src/helpers/mostForks.js
+++ client/src/helpers/mostForks.js
@@ -1,68 +1,22 @@
-const tree = [
-  {
-    id: 2,
-    parent: 1,
-    path: [1],
-  },
-  {
-    id: 4,
-    parent: 1,
-    path: [1],
-  },
-  {
-    id: 3,
-    parent: 2,
-    path: [1, 2],
-  },
-  {
-    id: 7,
-    parent: 4,
-    path: [1, 4],
-  },
-  {
-    id: 6,
-    parent: 3,
-    path: [1, 2, 3],
-  },
-  {
-    id: 5,
-    parent: 3,
-    path: [1, 2, 3],
-  },
-  {
-    id: 6,
-    parent: 3,
-    path: [1, 2, 3],
-  },
-];
-
 function mostForkedRecipeId(treeData) {
-  if (treeData.length === 1) {
-    console.log('onlyOneId');
+  if (treeData.length === 1 && treeData[0].parent === null) {
     const rId = treeData[0]._id;
     return rId;
   }
-  console.log('mF treedata', treeData);
   //aggregate
   const pIdArr = [];
   treeData.map((dataPoint) => pIdArr.push(dataPoint.parent));
-  console.log('mf pIdArr', pIdArr);
   //count occurences
   const pIdCounts = {};
   for (const pId of pIdArr) {
     pIdCounts[pId] = pIdCounts[pId] ? pIdCounts[pId] + 1 : 1;
   }
-  console.log('mF pIdCounts', pIdCounts);
   //get max
   const arrOfOccurences = Object.values(pIdCounts);
   const max = Math.max(...arrOfOccurences);
-  console.log('mF max', max);
   //return key with highest value
   const pId = Object.keys(pIdCounts).find((key) => pIdCounts[key] === max);
-  console.log('mF pId', pId);
   return pId;
 }
-
-console.log(mostForkedRecipeId(tree));
 
 export default mostForkedRecipeId;
