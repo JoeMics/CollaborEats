26d9f24d4810abd1d413635f372a1bcf012812c8
Joseph Micla <josephmicla@gmail.com>
2022-02-05 15:12:49 -0800

    sets whole user info instead of ID

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,15 +1,23 @@
+import { useEffect } from 'react';
 import { useState, createContext } from 'react';
+import { fetchCurrentUser } from '../services/api';
 
 const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  // This should ONLY be used for demo purposes
-  // Ideally, authentication should be done using cookies or some other method
-  // In the MVD functionality, we should be able to switch between TWO IDs:
-  // 61e607f0311d699fd35f509e - JoeMics@example.com
-  // 61e608607f04825b4c4cd517 - IvanTang@avatar.com
-  const [userId, setUserId] = useState('61e607f0311d699fd35f509e');
-  const authInfo = { userId, setUserId };
+  const [user, setUser] = useState('');
+  const authInfo = { user, setUser };
+
+  // Allows user to stay logged in after refresh
+  // receives user from server, after checking active sessions
+  useEffect(() => {
+    async function fetchUser() {
+      const result = await fetchCurrentUser();
+      setUser(result.data);
+    }
+
+    fetchUser();
+  }, []);
 
   return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
