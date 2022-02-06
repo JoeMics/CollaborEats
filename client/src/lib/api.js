e07e7bec02724bb9f8417051f9fcd9fd472ae5e9
Joseph Micla <josephmicla@gmail.com>
2022-02-05 20:46:35 -0800

    change baseURL based on .env

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Use with credntials in order to store cookies from server
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
 
