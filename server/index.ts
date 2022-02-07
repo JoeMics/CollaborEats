0bceee7b6e69fabb9da4218d74a611847c7610b3
Ivan <ivanxintang@gmail.com>
2022-02-07 03:05:15 -0800

    change cors options

--- server/index.ts
+++ server/index.ts
@@ -74,12 +74,12 @@ declare module 'express-serve-static-core' {
 // the User data is accessible on every endpoint as "req.user"
 app.use(async (req, res, next) => {
   // Prevent cors
-  res.header('Access-Control-Allow-Origin', process.env.WEB_APP_URL);
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
+  // res.header('Access-Control-Allow-Origin', process.env.WEB_APP_URL);
+  // res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  // res.header(
+  //   'Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  // );
 
   // Check for access token in header
   // Token must be of authorization type, and must include "Bearer"
