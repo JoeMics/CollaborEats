0f45a42d3f9ff0173541336b1b2819291870bae6
Ivan <ivanxintang@gmail.com>
2022-02-07 02:08:09 -0800

    Access-Control-Allow headers

--- server/index.ts
+++ server/index.ts
@@ -60,6 +60,7 @@ declare module 'express-serve-static-core' {
 app.use(async (req, res, next) => {
   // Prevent cors
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
