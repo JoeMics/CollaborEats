c9face81ace8676c9d447a941241cafba7f6799b
Ivan <ivanxintang@gmail.com>
2022-02-07 01:11:41 -0800

    Re-enable cors to see initial error

--- server/index.ts
+++ server/index.ts
@@ -44,7 +44,8 @@ app.use(
 
 // Add origin, and credentials to receive session from client
 // disable cors
-app.use(cors({ origin: false }));
+// app.use(cors({ origin: false }));
+app.use(cors({ origin: process.env.WEB_APP_URL, credentials: true }));
 // app.use(methodOverride('_method'));
 
 // Use declaration merging to add user and userId
@@ -59,8 +60,8 @@ declare module 'express-serve-static-core' {
 // the User data is accessible on every endpoint as "req.user"
 app.use(async (req, res, next) => {
   // Prevent cors
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   // Check for access token in header
   // Token must be of authorization type, and must include "Bearer"
