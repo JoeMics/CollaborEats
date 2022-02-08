6218d1c7cd2f3629d3c945d12e6f5c71cb4e2039
Ivan <ivanxintang@gmail.com>
2022-02-08 15:27:52 -0800

    Add image validation

--- client/src/pages/ImageUpload.jsx
+++ client/src/pages/ImageUpload.jsx
@@ -6,27 +6,23 @@ const ImageUpload = () => {
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false);
   const [imgurl, setImgurl] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    const url = await getSignedURL();
-    const newUrl = await uploadS3Img(file, url);
-    setImgurl(newUrl);
-    setLoading(false);
-    // const formData = new FormData();
-    // formData.append('file', file);
-    // try {
-    //   const response = await axios({
-    //     method: 'post',
-    //     url: 'http://localhost:8080/upload',
-    //     data: formData,
-    //     headers: { 'Content-Type': 'multipart/form-data' },
-    //   });
-    //   console.log('the file name: ', response.data.file.filename);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    if (!file.name.match(/.(jpg|jpeg|png)$/i)) {
+      setError('NOT AN IMAGE');
+    }
+    if (file.size < 5 * 1024 * 1024) {
+      setLoading(true);
+      const url = await getSignedURL();
+      const newUrl = await uploadS3Img(file, url);
+      setImgurl(newUrl);
+      setLoading(false);
+    } else {
+      setError('file size exceeded');
+    }
   };
 
   const handleFileSelect = (e) => {
@@ -50,7 +46,13 @@ const ImageUpload = () => {
           <h1 className="font-bold text-xl text-center my-4">Upload Files</h1>
           <form onSubmit={handleSubmit}>
             <div className="mb-3">
-              <input type="file" name="file" id="file" onChange={handleFileSelect} />
+              <input
+                type="file"
+                name="file"
+                id="file"
+                onChange={handleFileSelect}
+                accept="image/png, image/jpeg"
+              />
               <label htmlFor="for">Choose File</label>
             </div>
             <input type="submit" value="Submit" className="px-3 bg-blue-400" />
@@ -58,6 +60,7 @@ const ImageUpload = () => {
           <button type="button" onClick={handleFetch}>
             get all images
           </button>
+          {error && <span>{error}</span>}
           <h3>THE IMAGE</h3>
           {!loading && <img src={imgurl} alt="asdf" />}
         </div>
