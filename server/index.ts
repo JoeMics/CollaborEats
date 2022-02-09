24e820e5f5c52151259a546ff07df41f3b1daa21
Joseph Micla <josephmicla@gmail.com>
2022-02-08 18:40:08 -0800

    remove old image upload routes

--- server/index.ts
+++ server/index.ts
@@ -1,37 +1,36 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Use declaration merging to add user and userId
+declare module 'express-serve-static-core' {
+  interface Request {
+    user: any;
+  }
+}
+
+// Constants
+const PORT = process.env.PORT || 8080;
+const { GOOGLE_APPLICATION_CREDENTIALS } = process.env;
+
 import express, { application } from 'express';
 import cors from 'cors';
 import { getAuth } from 'firebase-admin/auth';
+import { generateUploadURL } from './s3';
 
 import User from './models/user';
-import Recipe from './models/recipe';
-import Comment from './models/comment';
 import userRoutes from './routes/user';
 import recipeRoutes from './routes/recipe';
 
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-import { generateUploadURL } from './s3';
-const path = require('path');
-const crypto = require('crypto');
-const multer = require('multer');
-const { GridFsStorage } = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
 const admin = require('firebase-admin');
-// for deleting photos @JoeMics
-// const methodOverride = require('method-override');
-
-// Constants
-const PORT = process.env.PORT || 8080;
 
 // Server
 const app = express();
 
 //Initialize firebase admin sdk
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!)),
+  credential: admin.credential.cert(JSON.parse(GOOGLE_APPLICATION_CREDENTIALS!)),
 });
 
 // Middleware
@@ -47,21 +46,13 @@ app.use(
 app.use(
   cors({
     // uncomment to work in dev environment
-    // origin: 'http://localhost:3000',
-    origin: 'https://vibrant-cray-95d891.netlify.app',
+    origin: 'http://localhost:3000',
+    // origin: 'https://vibrant-cray-95d891.netlify.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Reqested-With', 'Accept'],
     preflightContinue: true,
   })
 );
-// app.use(methodOverride('_method'));
-
-// Use declaration merging to add user and userId
-declare module 'express-serve-static-core' {
-  interface Request {
-    user: any;
-  }
-}
 
 // middleware to check current user on every request
 // update Users collection on every request
@@ -112,123 +103,10 @@ mongoose
   })
   .catch((err: any) => console.log(`Could not connect due to ${err}`));
 
-// Initalize gfs
-// @ts-ignore:next-line
-let gfs;
-
-const conn = mongoose.connection;
-
-conn.once('open', () => {
-  // This initializes the stream
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('images');
-});
-
-// This is the storage engine
-const storage = new GridFsStorage({
-  url: process.env.DB_URI,
-  file: (req: any, file: any) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err: any, buf: any) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'images',
-        };
-        resolve(fileInfo);
-      });
-    });
-  },
-});
-const upload = multer({ storage });
-
 app.get('/s3upload', async (req, res) => {
   const url = await generateUploadURL();
   res.send(url);
   console.log('signed: ', url);
-});
-
-app.get('/upload', (req, res) => {
-  // @ts-ignore:next-line
-  gfs.files.find().toArray((err, files) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      res.send({ files: false });
-    } else {
-      // @ts-ignore:next-line
-      files.map((file) => {
-        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-          file.isImage = true;
-        } else {
-          file.isImage = false;
-        }
-      });
-      res.send({ files: files });
-    }
-  });
-});
-app.post('/upload', upload.single('file'), (req, res) => {
-  console.log(req.file);
-  res.json({ file: req.file });
-});
-
-app.get('/files', (req, res) => {
-  // @ts-ignore:next-line
-  gfs.files.find().toArray((err: any, files: any) => {
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: 'No files exist',
-      });
-    }
-    return res.json(files);
-  });
-});
-
-app.get('/files/:filename', (req, res) => {
-  // @ts-ignore:next-line
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists',
-      });
-    }
-    return res.json(file);
-  });
-});
-
-// @route GET /image/:filename
-// @desc Display Image
-app.get('/image/:filename', (req, res) => {
-  // @ts-ignore:next-line
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists',
-      });
-    }
-
-    // Check if image
-    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-      const bucket = new mongoose.mongo.GridFSBucket(conn, { bucketName: 'images' });
-      let readStream = bucket.openDownloadStream(file._id);
-      readStream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: 'Not an image',
-      });
-    }
-  });
-});
-
-app.get('/', async (req, res) => {
-  const users = await User.find();
-  const comments = await Comment.find();
-  const recipes = await Recipe.find();
-  res.send({ users, comments, recipes });
 });
 
 app.listen(PORT, () => console.log(`API server running on port: ${PORT}`));
